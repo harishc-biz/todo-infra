6513fa03eb536766477125e419db3315cbffb8c7
Harisha C <harishac@Harishas-MacBook-Pro.local>
2025-01-31 20:16:55 +0530

    updated terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,12 @@
 # Resource Group
 data "azurerm_resource_group" "rg" {
   name     = "rg-todo-dev"
-  location = var.location
 }
 
 resource "azurerm_container_registry" "acr" {
   
   name                     = "acrtodo${var.environment}"
-  resource_group_name      = azurerm_resource_group.rg.name
+  resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = var.location
   sku                      = "Basic"
   admin_enabled            = true
@@ -20,7 +19,7 @@ module "aks" {
   version = "9.3.0"
 
   cluster_name = "aks-todo-list-${var.environment}"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
 
 }
