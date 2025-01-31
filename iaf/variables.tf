5f928a2a6f6f94ec10b8f2adb606a5348ed9316f
Harisha C <harishac@Harishas-MacBook-Pro.local>
2025-01-31 18:06:40 +0530

    Added infra code

---
+++ iaf/variables.tf
@@ -0,0 +1,30 @@
+variable "environment" {
+  description = "The environment (dev, qa, prod)"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string
+}
+
+variable "container_name" {
+  description = "The name of the container"
+  type        = string
+}
+
+variable "key" {
+  description = "The key for the state file"
+  type        = string
+}
