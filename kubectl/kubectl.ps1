55bbf93385b5618d13266ae4ef16657a15daa344
harishc-biz <harishdravid9845@gmail.com>
2025-02-02 18:29:27 +0530

    Azure infra destroy

--- kubectl/kubectl.ps1
+++ kubectl/kubectl.ps1
@@ -3,10 +3,10 @@ kubectl create ns todo-app
 
 #to create a new secret
 kubectl create secret docker-registry acr-auth \
-  --docker-server=<acr-name>.azurecr.io \
-  --docker-username=<service-principal-id> \
-  --docker-password=<service-principal-password>
+  --docker-server=acrtododev.azurecr.io \
+  --docker-username=acrtododev \
+  --docker-password=iDOhN+ECrbUDg40w9F3LlzAVZefM6fMtgABtdlA3cH+ACRAiXTvX -n todo-app
 
 #ArgoCD installation
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f kubectl/ArgoCD/install.yml
